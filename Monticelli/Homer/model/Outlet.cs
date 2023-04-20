3313dc3e618177641ed0b5d7f57a70eba08ef76d
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-04-20 11:19:22 +0200

    Implement IOutlet interface

---
+++ Monticelli/Homer/model/Outlet.cs
@@ -0,0 +1,33 @@
+namespace Homer.model;
+
+public class Outlet : IOutlet
+{
+
+    private OutletState _state;
+
+    public Outlet(readonly OutletState state)
+    {
+        _state = state;
+    }
+
+    public Outlet(Outlet outlet)
+    {
+        _state = outlet.State;
+    }
+
+    public OutletState State
+    {
+        get => _state;
+
+
+        set => _state = value;
+    }
+
+    public void UpdateTick(TimeSpan timeSpan)
+    {
+        double randomIncrement = Math.sin(timeSpan.Seconds);
+        this.State += randomIncrement;
+
+    }
+
+}
