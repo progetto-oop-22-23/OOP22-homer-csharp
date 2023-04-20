40dc06d73e1173f896ad25404a8a9fe185fee616
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-04-20 11:26:46 +0200

    Implement IOutletState

---
+++ Monticelli/Homer/model/OutletState.cs
@@ -0,0 +1,36 @@
+namespace homer.model;
+
+public class OutletState : IOutletState
+{
+    private double _minConsumption;
+    private double _maxConsumption;
+    private double _consumption;
+
+    public OutletState(double minConsumption, double maxConsumption, double consumption)
+    {
+        _minConsumption = minConsumption;
+        _maxConsumption = maxConsumption;
+        _consumption = consumption;
+    }
+
+    public double MinConsumption
+    {
+        get => _minConsumption;
+
+        set => _minConsumption = value;
+    }
+
+    public double MaxConsumption
+    {
+        get => _maxConsumption;
+
+        set => _maxConsumption = value;
+    }
+
+    public double Consumption
+    {
+        get => _consumption;
+
+        set => _consumption = value;
+    }
+}
