7e336443176308f8a147eb0677759cf5ab317bd0
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-04-20 11:26:35 +0200

    OutletState interface

---
+++ Monticelli/Homer/model/IOutletState.cs
@@ -0,0 +1,8 @@
+namespace Homer.model;
+
+public interface IOutletState
+{
+    public double MinConsumption { get; set; }
+    public double MaxConsumption { get; set; }
+    public double Consumption { get; set; }
+}
